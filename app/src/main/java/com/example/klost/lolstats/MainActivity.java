e3d1a778069375f209c7cddbd3be537e72b77bfa
Diego <dariag00@estudiantes.unileon.es>
2018-10-09 20:44:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/klost/lolstats/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.klost.lolstats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
